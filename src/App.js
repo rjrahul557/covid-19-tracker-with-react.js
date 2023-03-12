024821e3cc7a329764bffc85ceaefa8708183b54
Rahul <rahulrjz600@gmail.com>
2023-03-13 02:41:08 +0530

    end commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ import React,{useState,useEffect} from 'react';
 import './App.css';
 import InfoBox from './InfoBox';
 import Map from './Map';
+import Table from './Table';
+import { sortData } from './util';
+import LineGraph from './LineGraph';
+import "leaflet/dist/leaflet.css";
 
 
 function App() {
   const [countries,setCountries] = useState([]);
   const [country,setCountry] = useState("worldwide");
+  const [countryInfo,setCountryInfo] = useState({});
+  const [tableData,setTableData] = useState([]);
+  const [mapCenter,setMapCenter] = useState({lat:34.80746,lng:-40.4796});
+  const [mapZoom,setMapZoom] = useState(3);
+  const [mapCountries,setMapCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then((response) => response.json())
+    .then((data) => {
+      setCountryInfo(data);
+    });
+  },[]);
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -19,7 +36,10 @@ function App() {
           name : country.country,
           value:country.countryInfo.iso2,
         }));
-
+        
+        const sortedData = sortData(data);
+        setTableData(sortedData);
+        setMapCountries(data);
         setCountries(countries);
        });
     };
@@ -27,10 +47,26 @@ function App() {
     getCountriesData();
   },[]);
 
-  const onCountryChange = (event) => {
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
 
-    setCountry(countryCode);
+    const url =
+    countryCode === "worldwide"
+    ? "https://disease.sh/v3/covid-19/all"
+    : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      setCountry(countryCode);
+
+      setCountryInfo(data);
+
+      setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+      setMapZoom(4);
+    });
+
+    //console.log(countryInfo);
 
   };
   return (
@@ -53,18 +89,34 @@ function App() {
           </FormControl>
         </div>
         <div className='app_stats'>
-          <InfoBox title="Coronavirus Cases" cases={1234} total={3000}/>
-          <InfoBox title="Recovered" cases={1234} total={3000}/>
-          <InfoBox title="Deaths" cases={1234} total={3000}/>
+          <InfoBox title="Coronavirus Cases" 
+            cases={countryInfo.todayCases} 
+            total={countryInfo.cases}
+          />
+
+          <InfoBox title="Recovered" 
+            cases={countryInfo.todayRecovered} 
+            total={countryInfo.recovered}
+          />
+          <InfoBox title="Deaths" 
+            cases={countryInfo.todayDeaths} 
+            total={countryInfo.deaths}
+          />
 
         </div>
         
-        <Map/>
+        <Map
+         countries = {mapCountries}
+         center = {mapCenter}
+         zoom = {mapZoom}
+        />
       </div>
       <Card className='app_right'>
         <CardContent>
-          <h3>live cases by country</h3>
-          <h3>worldwide new cases</h3>
+          <h3>Live cases by country</h3>
+          <Table countries = {tableData}/>
+          {/* <h3>worldwide new cases</h3> */}
+          <LineGraph />
         </CardContent>
       </Card>
       
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
-function Map() {
+import React from "react";
+import "./Map.css";
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { showDataOnMap } from "./util";
+function Map({countries,casesType,center,zoom}) {
   return (
     <div className='map'>
-      <h1>i m a map</h1>
+       <MapContainer center={center} zoom={zoom}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {showDataOnMap(countries,casesType)}
+       </MapContainer>
     </div>
   )
 }
